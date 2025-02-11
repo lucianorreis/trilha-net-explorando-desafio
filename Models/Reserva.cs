55f05d33fbedb3e7972242fbdd6360196db30cd2
Luciano.Reis <lrr.arms233@gmail.com>
2025-02-10 22:18:13 -0300

    Novo Desafio - Trilha .NET
    Implementação de reserva de hotel
    
    Falta implementar o cálculo de desconto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -5,7 +5,6 @@ namespace DesafioProjetoHospedagem.Models
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
-
         public Reserva() { }
 
         public Reserva(int diasReservados)
@@ -15,16 +14,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            //*  Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                
+                throw new Exception("O número de hospedes não deve ser maior que a capacidade da suite");
             }
         }
 
@@ -35,23 +33,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            //* Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
+            //* Retorna o valor da diária
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            int valorDiaria = Convert.ToInt32(Suite.ValorDiaria);
+            
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valorDiaria - (valorDiaria /100);
             }
 
             return valor;
